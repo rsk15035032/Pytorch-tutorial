2869e41c569f03542e51ae3013a61f5ec723ea77
Ravi Shankar Kumar <rsk15035032@gmail.com>
2026-01-19 01:32:02 +0530

    Iniatial commit of 1st NN

---
+++ nuralNetwork.py
@@ -0,0 +1,29 @@
+# Import 
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+
+# Create a fully connected network
+
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+
+# Set a device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
