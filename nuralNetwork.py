69233e1fbfd572f98e8a1d715afdb6be52522966
Ravi Shankar Kumar <rsk15035032@gmail.com>
2026-01-20 12:57:23 +0530

    Full fleged trained model

--- nuralNetwork.py
+++ nuralNetwork.py
@@ -27,3 +27,76 @@ class NN(nn.Module):
 # Set a device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+#Hyperparameter
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 1
+
+#Load data
+train_dataset = datasets.MNIST(root= 'dataset/',train= True, transform=transforms.ToTensor(),download=True )
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = datasets.MNIST(root= 'dataset/',train= False, transform=transforms.ToTensor(),download=True )
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+
+# Initialize network
+model = NN(input_size=input_size, num_classes=num_classes).to(device)
+
+# Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+
+#train networks
+
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get data to cuda if possible
+        data = data.to(device=device)
+        targets= targets.to(device=device)
+
+        # Get to correct shape
+        data = data.reshape(data.shape[0], -1)
+        
+        # forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # backwards
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradient descent or Adam steps
+        optimizer.step()
+
+# Check accuracy on train and test sets to see how good our model is.
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print("checking accuracy on traing datasets")
+    else:
+        print("checking accuracy on test data")
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device = device)
+            y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct = (predictions == y).sum()
+            num_samples = predictions.size(0)
+
+        print(f'Got (num_correct)/(num_samples) with an accuracy {(num_correct)/(num_samples)*100:.2f}')
+    model.train()
+    
+    
+check_accuracy(train_loader,model)
+check_accuracy(test_loader,model)
+
+
